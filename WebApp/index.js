566594c0141c5665b03f125290dad7556f6eab34
lazuta <leonid.lazuta@mail.ru>
2021-03-31 11:58:27 +0300

    Step 4: XML Views

--- WebApp/index.js
+++ WebApp/index.js
@@ -1,11 +1,17 @@
 sap.ui.define([
-	"sap/m/Text"
+	// "sap/m/Text",
+    "sap/ui/core/mvc/XMLView"
 
-], function (Text) {
+], function (XMLView) {
 	"use strict";
 
-	new Text({
-		text: "text"
-	}).placeAt("content");
+	// new Text({
+	// 	text: "text"
+	// }).placeAt("content");
 
+    XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
 });
